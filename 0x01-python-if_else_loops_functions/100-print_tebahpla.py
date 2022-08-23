c8282a8f6c1bab91dc4e03574a68a2d114835055
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-08-23 21:13:58 +0000

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    print('{}'.format(chr(i - 32) if (i % 2 != 0) else chr(i)), end='')
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        print("{:c}".format(i - 32), end='')
+    else:
+        print("{:c}".format(i), end='')
