246f44a833c01d599da5e7a7c0da7010fe705051
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-08-31 12:03:42 +0000

    Present in both

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    result = 0
-    for i in range(1, len(sys.argv)):
-        result += int(sys.argv[i])
-    print("{}".format(result)
+def common_elements(set_1, set_2):
+    common_e = set_1 & set_2
+    return common_e
