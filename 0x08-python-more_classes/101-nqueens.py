2229570db467145a13963e74ed073da3891c1a10
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-09-19 20:38:20 +0000

    N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,4 +1,4 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 """
 nqueens backtracking program to print the coordinates of n queens
 on an nxn grid such that they are all in non-attacking positions
@@ -60,4 +60,3 @@ if __name__ == "__main__":
 
     # start the recursive process at x = 0
     nqueens(0)
-
