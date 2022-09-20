e6099239859cbd875f9e53a74a4e82162baa4e42
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-09-20 19:50:31 +0000

    Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 class LockedClass:
-    def __setattr__(self, attr, value):
-        if attr != 'first_name':
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(attr))
-        self.__dict__.update({attr: value})
+    """prevent the dynamic creation of attributes"""
+    __slots__ = ['first_name']
+
