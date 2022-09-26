4a92f725e0442bb2d14d4cb72771d715bf5e82d1
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-09-26 14:35:10 +0000

    Full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
+'''Python in HOlberton'''
 
-r = Rectangle(3, 5)
 
-print(r)
-print(r.area())
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
+class Rectangle(BaseGeometry):
+    '''Rectangle class inherited from BaseGeometry'''
+    def __init__(self, width, height):
+        '''Instantiates the object'''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        '''Set __str___ value'''
+        string = "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+        return string
+
+    def area(self):
+        '''Calculate and returns rectangle's area'''
+        return self.__width * self.__height
