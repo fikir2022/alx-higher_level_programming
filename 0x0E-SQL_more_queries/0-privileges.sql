df18781a61dc42c220029799c1715c354f88cee2
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-11-04 14:14:10 +0000

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
-st all privileges for users 'user_0d_1' and 'user_0d_2'
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
+
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
