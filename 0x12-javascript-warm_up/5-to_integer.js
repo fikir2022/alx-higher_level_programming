b78856ebf6f406a133bf80fba4cdd070648fd2ca
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-11-29 09:04:17 +0000

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,2 @@
 #!/usr/bin/node
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+console.log(process.argv[2] + 'is' + process.argv[3]);
