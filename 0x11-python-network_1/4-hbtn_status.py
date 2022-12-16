3bb17e1d8efe7a73a7d9842175d4b49f1e7f48e0
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-12-16 01:54:43 -0800

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     r = requests.get('https://intranet.hbtn.io/status')
     text = r.text
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
