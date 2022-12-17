0fdfece809cbbf1f3333ca735f4e2bb1d4005e05
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-12-17 04:12:35 -0800

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Script that fetches https://intranet.hbtn.io/status and
-prints information about the body of the response retrieved.
 """
-from urllib.request import (urlopen, Request)
+Fetches https://intranet.hbtn.io/status with urllib
+"""
+
+
+import urllib.request
 
 
 if __name__ == "__main__":
-
-    req = Request('https://intranet.hbtn.io/status')
-    with urlopen(req) as response:
-        data = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(data)))
-        print('\t- content: {}'.format(data))
-        print('\t- utf8 content: {}'.format(data.decode('utf-8')))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode()))
