e8b4fac99fede6ee4ad4e62e2dfe7801aae33726
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-12-17 04:31:37 -0800

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Module that fetches https://alx-intranet.hbtn.io/status
-    """
+"""
+Module that fetches https://alx-intranet.hbtn.io/status
+
+"""
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
