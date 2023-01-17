97008446c441ce0c7c6c4cc5e4715a4ad62a463d
Fikir Aliya <fikirtesfaye2@gmail.com>
2023-01-17 21:13:02 +0000

    Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,16 +1,4 @@
 #!/usr/bin/node
-
-const request = require('request');
 const fs = require('fs');
-
-request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    fs.writeFile(process.argv[3], body, 'utf-8', function (err) {
-      if (err) {
-        console.log(err);
-      }
-    });
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
