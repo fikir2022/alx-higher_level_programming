c31b8db2440d3a613e79e146becf54c1c7586aca
Fikir Aliya <fikirtesfaye2@gmail.com>
2023-01-17 20:57:08 +0000

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = process.argv[2];
-request(url, function (err, response) {
+const URL = process.argv[2];
+
+request(URL, function (err, response) {
   if (err) {
     console.log(err);
   } else {
